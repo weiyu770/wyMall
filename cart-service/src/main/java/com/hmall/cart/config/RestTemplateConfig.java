d993725bff0f9a050bca8f5f413fe7580748befa
yu_wei <3658043236@qq.com>
2024-12-14 00:37:14 +0800

    第一次提交

---
+++ cart-service/src/main/java/com/hmall/cart/config/RestTemplateConfig.java
@@ -0,0 +1,16 @@
+package com.hmall.cart.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        // 使用 RestTemplateBuilder 配置 RestTemplate
+        return restTemplateBuilder.build();
+    }
+}
---
+++ item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -0,0 +1,15 @@
+package com.hmall.item;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@MapperScan("com.hmall.item.mapper")
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApplication.class, args);
+    }
+}
